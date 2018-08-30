8f7ba1a1cd280ef7f0e25d49e8c4c7a23a6a1a60
ybh0822 <by58@cornell.edu>
2018-08-29 23:24:27 -0700

    Add HW2

---
+++ Homework/HW2/tests/q1.py
@@ -0,0 +1,22 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> x
+          91
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
